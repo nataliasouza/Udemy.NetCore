7f0b910a9db1c15d0bc6b7f470e50bfa049ceb8e
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-05 16:21:26 -0300

    Operadores Aritméticos

---
+++ OperadoresAritmeticos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+            double n4 = 10.0 / 8.0;
+
+            //variaveis para usar na fórmula de Bhaskara
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            //RAIZ b² - 4ac = delta | Cria uma var p/ receber o resultado
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            //RAIZ = Math.Sqrt
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
