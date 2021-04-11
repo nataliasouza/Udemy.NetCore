9f117786a837169ca8c973e6416c550792722eec
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-11 20:02:47 -0300

    Exercício - FOR

---
+++ ExercicioFor/ExercicioFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExercícioFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos numeros inteiros você irá digitar?");
+            int num = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write("\nValor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+
+                soma += valor;
+            }
+            Console.Write("\nA soma é: " + soma);
+        }
+    }
+}
