4aadc48e1b24b4b489e6cecd2a21d2c56311c2f3
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-10 22:54:17 -0300

    Exercicio While

---
+++ ExercicioWhile/ExercicioWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while ( num >= 0.0)
+            {
+                double raiz = Math.Sqrt(num);
+                Console.WriteLine(raiz.ToString("F3",CultureInfo.InvariantCulture));
+                Console.WriteLine("Digite outro número: ");
+                num = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Número Negativo!");
+
+
+        }
+    }
+}
