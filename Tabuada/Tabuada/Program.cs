b4041090e12a22086ff068e97d3320141840e7b9
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-11 20:30:55 -0300

    Tabuada

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para visualizar a Tabuada: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n***** TABUADA DE MULTIPLICAÇÃO DO NÚMERO: {num} *****\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = num * i;
+                Console.WriteLine(num + " x " + i + " = " + resultado);
+            }
+            Console.WriteLine("\n***** FIM DA TABUADA *****");
+        }
+    }
+}
