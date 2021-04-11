0dc4dcec0c434b9b7f6cc763cc43be46be7899cd
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-10 23:36:48 -0300

    Crescente e Decrescente

---
+++ CrescenteDecrescente/CrescenteDecrescente/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CrescenteDecrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.Write("Digite dois número e os separe com espaço: ");
+            string[] numeros = Console.ReadLine().Split(' ');
+
+            num1 = int.Parse(numeros[0]);
+            num2 = int.Parse(numeros[1]);
+
+            while (num1 != num2)
+            {
+                if (num1 < num2)
+                {
+                    Console.Write("Crescente");
+                }
+                else
+                {
+                    Console.Write("Decrescente");
+                }
+
+                Console.Write("\nDigite dois número e os separe com espaço: ");
+                numeros = Console.ReadLine().Split(' ');
+
+                num1 = int.Parse(numeros[0]);
+                num2 = int.Parse(numeros[1]);
+            }
+
+        }
+    }
+}
