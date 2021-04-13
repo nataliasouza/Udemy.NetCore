5fafb2bebd59523f5e169326be18e8bf4ecbcd58
nataliasouza <nataliasouza.ti@hotmail.com>
2021-04-12 22:14:43 -0300

    Estoque

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //sobreposição 
+        public override string ToString()
+        {
+            return Nome + " , $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Estoque/Estoque/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+
+            Produto p = new Produto();
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Dados do produto: " + p);
+        }
+    }
+}
