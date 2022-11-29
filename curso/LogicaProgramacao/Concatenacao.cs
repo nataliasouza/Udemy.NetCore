b122bd475a76482e02d573daf83e32b8a3b64dea
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-11-29 20:38:01 -0300

    Recapitulando Lógica Programação - Exercício Placeholders, Interpolação e Concatenação

---
+++ curso/LogicaProgramacao/Concatenacao.cs
@@ -0,0 +1,21 @@
+﻿//Placeholders, interpolação e concatenação
+
+using System.Globalization;
+
+namespace curso.LogicaProgramacao
+{
+    public class Concatenacao
+    {        
+        public static void Executar()
+        {
+            int idade = 37;
+            double saldo = 13000.8000;
+            string nome = "Max";
+
+            Console.WriteLine("{0} tem {1} anos e seu saldo é: {2:F2} reais", nome, idade, saldo);
+            Console.WriteLine($"{nome} tem {idade} anos e seu saldo é: {saldo:F2} reais");
+            Console.WriteLine(nome + " tem " + idade + " anos e seu saldo é: " 
+                + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    }
+}
--- curso/Program.cs
+++ curso/Program.cs
@@ -5,6 +5,7 @@ var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 
                 //LÓGICA DE PROGRAMAÇÃO
                 {"Conta Bancária - Recapitulando Lógica de Programação", ContaBancaria.Executar},               
+                {"Placeholders, concatenação e interpolação - Recapitulando Lógica de Programação", Concatenacao.Executar},               
                                              
 
                 // PROGRAMAÇÃO ORIENTADA A OBJETO
