0a33a8fbb0b5b6c195d43f485eb43579f16d5a1b
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-01-10 14:34:19 -0300

    Exercicio Proposto - Parte 2 - Exercício 03

---
+++ curso/LogicaProgramacao/ExerciciosPropostos/Exercicio03.cs
@@ -0,0 +1,29 @@
+﻿
+//Leia dois valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "São multiplos"
+//ou "Não são multiplos", indicando se os valores lidos são multiplos entre si.
+//ATENÇÃO: os números devem poder ser digitados em ordem crescente ou decrescente.
+
+namespace curso.LogicaProgramacao.ExerciciosPropostos
+{
+    public class Exercicio03
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Digite o valor de A e B: ");
+            string letras = Console.ReadLine();
+
+            string[] multiplos = letras.Split(' ');
+            int a = int.Parse(multiplos[0]);
+            int b = int.Parse(multiplos[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("São multiplos!");
+            }
+            else
+            {
+                Console.WriteLine("Não são multiplos!");
+            }
+        }
+    }   
+}
