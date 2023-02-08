2e3b9442dfc7e5df6cac117331643f62ae0715da
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-02-08 13:11:35 -0300

    Estrutura Sequencial - Exercício 04

---
+++ curso/LogicaProgramacao/ExerciciosPropostos/EstruturaSequencial/Sequencial04.cs
@@ -0,0 +1,35 @@
+﻿//Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+//o valor que recebe por hora e calcula o salário desse funcionário. A seguir,
+//mostre o número e o salário do funcionário, com duas casas decimais.
+
+using System.Globalization;
+
+namespace curso.LogicaProgramacao.ExerciciosPropostos.EstruturaSequencial
+{
+    public class Sequencial04
+    {
+        public static void Executar()
+        {
+            Console.Write("Digite o código do Funcionário: ");
+            int codFuncionario = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de horas trabalhadas: ");
+            double horasTrabalhadas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor pago em horas: ");
+            double valorHoraFuncionario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double totalReceber = horasTrabalhadas * valorHoraFuncionario;
+            Console.Clear();
+
+            Console.WriteLine("=================== CALCULAR HORAS TRABALHADAS ================\n");
+            Console.WriteLine($"Horas Trabalhadas: {horasTrabalhadas}" );
+            Console.WriteLine($"Valor por Hora: {valorHoraFuncionario.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nCódigo do funcionário: {codFuncionario} e " +
+                $"o valor a receber R$: {totalReceber.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.ResetColor();
+            Console.WriteLine("\n=============================================================");
+        }
+    }
+}
--- curso/Program.cs
+++ curso/Program.cs
@@ -15,6 +15,7 @@ var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1003 - Exercício 1", Sequencial01.Executar},
                 {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1002 - Exercício 2", Sequencial02.Executar},
                 {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1007 - Exercício 3", Sequencial03.Executar},
+                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1008 - Exercício 4", Sequencial04.Executar},
                 {"Ímpar ou par - Recapitulando Lógica de Programação", ImparPar.Executar},
                 {"Operadores Comparativos - Recapitulando Lógica de Programação", OpComparativos.Executar},
                 {"Operadores Lógicos - Recapitulando Lógica de Programação", OpLogicos.Executar},
