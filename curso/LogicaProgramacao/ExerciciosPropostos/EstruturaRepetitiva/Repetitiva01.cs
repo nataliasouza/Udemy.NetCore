dbf2f45fce1c9c0e2d8f84aeb42bb80eba7733c2
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-02-09 17:10:10 -0300

    Estrutura Repetitiva - Exercício 01

---
+++ curso/LogicaProgramacao/ExerciciosPropostos/EstruturaRepetitiva/Repetitiva01.cs
@@ -0,0 +1,70 @@
+﻿//Escreva um programa que repita a leitura de uma senha até que ela seja válida.
+//Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida".
+//Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido"
+//e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+
+//ENTRADA ------------------------------------- SAIDA
+//2200    ------------------------------------- Senha Invalida
+//1020    ------------------------------------- Senha Invalida
+//2022    ------------------------------------- Senha Invalida
+//2002    ------------------------------------- Acesso Permitido
+
+/// <summary>
+/// 
+/// Observação neste exercício considerei que é necessário cadastrar uma senha
+/// e não usar a senha com valor fixo! Para testar com os valores de entrada acima,
+/// faça o cadastro da senha com o valor de entrada!
+/// 
+/// </summary>
+
+namespace curso.LogicaProgramacao.ExerciciosPropostos.EstruturaRepetitiva
+{
+    public class Repetitiva01
+    {
+        public static void Executar()
+        {
+            string senhaDigitada, senhaCadastrada;
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("===========================================");
+            Console.WriteLine("============ Cadastro de senha ============");
+            Console.WriteLine("===========================================\n");            
+            Console.ResetColor();
+            Console.Write("Digite uma senha: ");           
+            senhaCadastrada = Console.ReadLine();             
+            Console.Clear();
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("===========================================");
+            Console.WriteLine("========== Autenticação de Senha ==========");
+            Console.WriteLine("===========================================\n");
+            Console.ResetColor();
+            Console.Write("Digite sua senha: ");
+            senhaDigitada = Console.ReadLine();
+            Console.WriteLine("===========================================");
+            Console.Clear();
+
+            while(senhaCadastrada != senhaDigitada)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Senha inválida!");
+                Console.ResetColor();
+                Console.Write("\nAperte enter para digitar a senha novamente!");
+                Console.ReadKey();                
+                Console.Clear();
+
+                Console.Write("\nDigite sua senha: ");
+                senhaDigitada = Console.ReadLine();
+                Console.Clear();
+            }
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nAcesso Pemitido!");
+            Console.ResetColor();
+        }
+
+    }
+}
--- curso/Program.cs
+++ curso/Program.cs
@@ -1,38 +1,40 @@
 ﻿using curso.LogicaProgramacao;
 using curso.LogicaProgramacao.ExerciciosPropostos.EstruturaCondicional;
+using curso.LogicaProgramacao.ExerciciosPropostos.EstruturaRepetitiva;
 using curso.LogicaProgramacao.ExerciciosPropostos.EstruturaSequencial;
 
 var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 
                 //LÓGICA DE PROGRAMAÇÃO
-                {"Conta Bancária - Recapitulando Lógica de Programação", ContaBancaria.Executar},               
-                {"Placeholders, concatenação e interpolação - Recapitulando Lógica de Programação", Concatenacao.Executar},               
+                {"Exercício Conta Bancária - Recapitulando Lógica de Programação", ContaBancaria.Executar},               
+                {"Exercício Placeholders, concatenação e interpolação - Recapitulando Lógica de Programação", Concatenacao.Executar},               
                 {"Exercício de Fixação 01 - Recapitulando Lógica de Programação", ExeFixacao01.Executar},               
-                {"Bhaskara - Recapitulando Lógica de Programação", Bhaskara.Executar},              
-                {"Split - Recapitulando Lógica de Programação", SplitPalavras.Executar},             
-                {"Split Parte 2 - Recapitulando Lógica de Programação", SplitParte2.Executar},
-                {"Estrutura Sequencial - Exercicio Fixacao", ExercicioFixacaoSequencial.Executar},
-                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1003 - Exercício 1", Sequencial01.Executar},
-                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1002 - Exercício 2", Sequencial02.Executar},
-                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1007 - Exercício 3", Sequencial03.Executar},
-                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1008 - Exercício 4", Sequencial04.Executar},
-                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1010 - Exercício 5", Sequencial05.Executar},
-                {"Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1012 - Exercício 6", Sequencial06.Executar},
-                {"Ímpar ou par - Recapitulando Lógica de Programação", ImparPar.Executar},
-                {"Operadores Comparativos - Recapitulando Lógica de Programação", OpComparativos.Executar},
-                {"Operadores Lógicos - Recapitulando Lógica de Programação", OpLogicos.Executar},
-                {"Estrutura Condicional - if/else - Recapitulando Lógica de Programação", EstruturaCondicional.Executar},
-                {"Estrutura Condicional (ESCOPO) - if/else - Recapitulando Lógica de Programação", Desconto.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - if/else - Número Negativo?", Exercicio01.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - uri1044 - São Multiplos?", Exercicio03.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - uri1046 - Duração do Jogo", Exercicio04.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - uri1038 - Exercício 5 - minha versão", Exercicio05.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - uri1037 - Exercício 6", Exercicio06.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - uri1041 - Exercício 7", Exercicio07.Executar},
-                {"Estrutura Condicional - Exercícios Propostos Parte 2 - uri1051 - Exercício 8", Exercicio08.Executar},
-                {"Funcoes - Exercício Qual Maior Número", Funcoes.Executar},
-                {"Estrutura Repetitiva WHILE - Exercício Raiz Quadrada", RaizQuadrada.Executar},
-                                             
+                {"Exercício Bhaskara - Recapitulando Lógica de Programação", Bhaskara.Executar},              
+                {"Exercício Split - Recapitulando Lógica de Programação", SplitPalavras.Executar},             
+                {"Exercício Split Parte 2 - Recapitulando Lógica de Programação", SplitParte2.Executar},
+                {"- Estrutura Sequencial - Exercicio Fixacao", ExercicioFixacaoSequencial.Executar},
+                {"- Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1003 - Exercício 1", Sequencial01.Executar},
+                {"- Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1002 - Exercício 2", Sequencial02.Executar},
+                {"- Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1007 - Exercício 3", Sequencial03.Executar},
+                {"- Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1008 - Exercício 4", Sequencial04.Executar},
+                {"- Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1010 - Exercício 5", Sequencial05.Executar},
+                {"- Estrutura Sequencial - Exercícios Propostos Parte 1 - uri1012 - Exercício 6", Sequencial06.Executar},
+                {"Exercício Exercício Ímpar ou par - Recapitulando Lógica de Programação", ImparPar.Executar},
+                {"Exercício Operadores Comparativos - Recapitulando Lógica de Programação", OpComparativos.Executar},
+                {"Exercício Operadores Lógicos - Recapitulando Lógica de Programação", OpLogicos.Executar},
+                {"- Estrutura Condicional - if/else - Recapitulando Lógica de Programação", EstruturaCondicional.Executar},
+                {"- Estrutura Condicional (ESCOPO) - if/else - Recapitulando Lógica de Programação", Desconto.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - if/else - Número Negativo?", Exercicio01.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - uri1044 - São Multiplos?", Exercicio03.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - uri1046 - Duração do Jogo", Exercicio04.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - uri1038 - Exercício 5 - minha versão", Exercicio05.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - uri1037 - Exercício 6", Exercicio06.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - uri1041 - Exercício 7", Exercicio07.Executar},
+                {"- Estrutura Condicional - Exercícios Propostos Parte 2 - uri1051 - Exercício 8", Exercicio08.Executar},
+                {"Exercício Funcoes - Exercício Qual Maior Número", Funcoes.Executar},
+                {"Exercício Raiz Quadrada - WHILE", RaizQuadrada.Executar},
+                {"- Estrutura Repetitiva WHILE - Exercícios Propostos Parte 3 - uri1114 - Exercício 1", Repetitiva01.Executar}
+                                        
 
                 // PROGRAMAÇÃO ORIENTADA A OBJETO
                 //{"Membros - Classes e Métodos", Membros.Executar},
