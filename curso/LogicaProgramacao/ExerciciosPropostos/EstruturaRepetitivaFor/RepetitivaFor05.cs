5482b18cfbceea465255ae9a23147bf4b6a1cf9f
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-02-12 16:25:32 -0300

    Estrutura Repetitiva FOR - Exercício 5 - Fatorial

---
+++ curso/LogicaProgramacao/ExerciciosPropostos/EstruturaRepetitivaFor/RepetitivaFor05.cs
@@ -0,0 +1,24 @@
+﻿//Ler um valor N. Calcular e escrever seu respectivo fatorial.
+//Fatorial de N = N * (N-1) *(N - 2) * (N - 3) * ... *1.
+//Lembrando que, por definição, fatorial de 0 é 1.
+
+namespace curso.LogicaProgramacao.ExerciciosPropostos.EstruturaRepetitivaFor
+{
+    public class RepetitivaFor05
+    {
+        public static void Executar()
+        {
+            Console.Write("Digite um número para calcular seu fatorial: ");
+            int numeroDigitado = int.Parse(Console.ReadLine());
+
+            int fatorial = 1;
+
+            for (int i = 1; i <= numeroDigitado; i++)
+            {
+                fatorial *= i;
+            }
+
+            Console.WriteLine(fatorial);
+        }
+    }
+}
