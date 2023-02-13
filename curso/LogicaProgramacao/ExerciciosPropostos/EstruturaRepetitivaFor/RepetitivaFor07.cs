f40944c4a3e6fb0476f4516a886431d82686c13e
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-02-13 17:53:20 -0300

    Estrutura Repetitiva FOR - Exercício 7 - valor de N ao quadrado e cubo

---
+++ curso/LogicaProgramacao/ExerciciosPropostos/EstruturaRepetitivaFor/RepetitivaFor07.cs
@@ -0,0 +1,27 @@
+﻿//Fazer um programa para ler um número inteiro positivo N. O programa deve então
+//mostrar na tela N linhas, começando de 1 até N. Para cada linha, mostrar o número
+//da linha, depois o quadrado e o cubo do valor, conforme exemplo.
+
+namespace curso.LogicaProgramacao.ExerciciosPropostos.EstruturaRepetitivaFor
+{
+    public static class RepetitivaFor07
+    {
+        public static void Executar()
+        {
+            Console.Write("Digite um número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int numero = i;
+                int quadrado = i * i;
+                int cubo = i * i * i;
+
+                Console.WriteLine($"\nNúmero: {numero} " +
+                    $"Quadrado: {quadrado} " +
+                    $"Cubo: {cubo}");
+            }
+        }
+    }
+}
