0c71ddfc986484da282e8af6cee8287b852d4613
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-02-13 19:27:00 -0300

    POO - Exercício Problema - Exemplo com orientação objetos - Calcular valor da área

---
+++ curso/POO/Triangulo.cs
@@ -0,0 +1,55 @@
+﻿//Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+//válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual
+//dos dois triângulos possui a maior área. A fórmula para calcular a área de um
+//triângulo a partir das medidas de seus lados a, b e c é a seguinte(fórmula de Heron):
+
+//Problema Exemplo - COM Orientação a Objeto.
+
+using System.Globalization;
+
+namespace curso.POO
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public static void Executar()
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
