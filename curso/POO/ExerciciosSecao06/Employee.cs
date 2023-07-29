f0a9924de48cf1eb6ea16ad3e3d7a762442557fc
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-07-29 20:20:04 -0300

    Refatoração - Exercício de Fixação - Listas

--- curso/POO/ExerciciosSecao06/Employee.cs
+++ curso/POO/ExerciciosSecao06/Employee.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Globalization;
+using System.Xml.Linq;
 
 namespace curso.POO.ExerciciosSecao06
 {
@@ -8,6 +9,7 @@ namespace curso.POO.ExerciciosSecao06
         public int Id { get; set; }
         public string Name { get; set; }
         public double Salary { get;  private set; }
+        public List<Employee> ListEmployees => listEmployees;
 
         public Employee(int id, string name, double salary)
         {
@@ -15,26 +17,14 @@ namespace curso.POO.ExerciciosSecao06
             Name = name;
             Salary = salary;
         }
+
+        private static List<Employee> listEmployees = new List<Employee>();
+
         public static void Executar()
         {
-            List<Employee> listEmployees = new List<Employee>();
             Console.Write("How many employees will be registered?: ");
-            int numberOfEmployees = int.Parse(Console.ReadLine()!);
-
-            for (int i = 0; i < numberOfEmployees; i++)
-            {
-                Console.WriteLine("\nEmployee #" + (i + 1) + ":");
-                Console.Write("Enter the Id: ");
-                int id = int.Parse(Console.ReadLine());
-
-                Console.Write("Enter the Name: ");
-                string name = Console.ReadLine();
-
-                Console.Write("Enter the Salary: ");
-                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                listEmployees.Add(new Employee(id, name, salary));
-            }           
+            int numberOfEmployees = int.Parse(Console.ReadLine()!);            
+            RegisterEmployee(numberOfEmployees);
 
             Console.Write("Enter the employee id that will have salary increase : ");
             int searchId = int.Parse(Console.ReadLine());
@@ -42,7 +32,7 @@ namespace curso.POO.ExerciciosSecao06
             Employee employeeId = listEmployees.Find(x => x.Id == searchId);
             if (employeeId != null)
             {
-                Console.Write("Enter the percentage: ");
+                Console.Write("\nEnter the percentage: ");
                 double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 employeeId.IncreaseSalary(percentage);
             }
@@ -56,7 +46,26 @@ namespace curso.POO.ExerciciosSecao06
             {
                 Console.WriteLine(name);
             }
-        }        
+        }
+
+        public static void RegisterEmployee(int numberOfEmployees)
+        {            
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine("\nEmployee #" + (i + 1) + ":");
+                Console.Write("Enter the Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter the Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                CreateEmployee(id,name,salary);
+            }
+        }
 
         public void IncreaseSalary(double percentage)
         {
@@ -65,11 +74,16 @@ namespace curso.POO.ExerciciosSecao06
 
         public override string ToString()
         {
-            return Id 
+            return Id
                 + ", "
                 + Name
                 + ", "
                 + Salary.ToString("F2", CultureInfo.InvariantCulture);
-        }   
+        }
+
+        public static void CreateEmployee(int id, string name, double salary)
+        {            
+            listEmployees.Add(new Employee(id, name, salary));
+        }
     }  
 }
